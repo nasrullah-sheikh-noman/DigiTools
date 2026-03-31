23df0c551d61262603ace3f9dce452ac740d8b23
Nasrullah Sheikh Noman <nasrullahsheikhnoman@gmail.com>
2026-03-31 10:32:37 +0600

    complete navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,11 @@
-import { FaShoppingCart } from "react-icons/fa";
+
 import "./App.css";
+import Navbar from "./components/navbar/Navbar";
 
 function App() {
   return (
     <>
-      <div className=" navbar bg-base-100 shadow-sm">
-        <div className="flex-1">
-          <a className="text-[#4F39F6] font-bold text-4xl">DigiTools</a>
-        </div>
-        <div>
-          <ul>
-            <a href="">Products</a>
-            <a href="">Features</a>
-            <a href="">Pricing</a>
-            <a href="">Testimonials</a>
-            <a href="">FAQ</a>
-          </ul>
-        </div>
-        <div>
-          <div>
-            <FaShoppingCart />
-            <button className="btn btn-active btn-primary">Get Started</button>
-          </div>
-        </div>
-      </div>
+      <Navbar/>
     </>
   );
 }
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { FaShoppingCart } from "react-icons/fa";
+
+const Navbar = () => {
+  return (
+    <div>
+      <div className="navbar bg-base-100 shadow-sm flex justify-between">
+          <div >
+            <a className="text-[#4F39F6] font-bold text-4xl">DigiTools</a>
+          </div>
+          <div>
+            <ul className="flex gap-4">
+              <li href="">Features</li>
+              <li href="">Products</li>
+              <li href="">Pricing</li>
+              <li href="">Testimonials</li>
+              <li href="">FAQ</li>
+            </ul>
+          </div>
+          <div>
+            <div className="flex justify-between items-center gap-4">
+              <span className="flex justify-between items-center gap-3">
+                <FaShoppingCart />
+                Login
+              </span>
+              <button className="btn btn-active btn-primary rounded-full">
+                Get Started
+              </button>
+            </div>
+          </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
