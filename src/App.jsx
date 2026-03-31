3954eb184dffdb2e9b742031ffdafe498a758220
Nasrullah Sheikh Noman <nasrullahsheikhnoman@gmail.com>
2026-03-31 13:18:25 +0600

    add premium and cart button

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import Navbar from "./components/navbar/Navbar";
 import Hero from "./components/hero/Hero";
 import Activity from "./components/activity/Activity";
+import ToggoleButton from "./components/toggoleButton/ToggoleButton";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Navbar/>
       <Hero/>
       <Activity/>
+      <ToggoleButton/>
     </>
   );
 }
