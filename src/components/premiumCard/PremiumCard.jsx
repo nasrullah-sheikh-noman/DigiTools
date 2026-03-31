81029492585d9f97d1b47a92593a58ebef4beb26
Nasrullah Sheikh Noman <nasrullahsheikhnoman@gmail.com>
2026-03-31 17:43:17 +0600

    show card in display using json data

--- src/components/premiumCard/PremiumCard.jsx
+++ src/components/premiumCard/PremiumCard.jsx
@@ -1,25 +1,29 @@
 import React, { use } from "react";
 
-const PremiumCard = () => {
-
-  // console.log("PremiumCardData", premimumCard);
+const PremiumCard = ({ cardData }) => {
+  console.log(cardData.title);
 
   return (
     <div>
-      <div className="card p-8 space-y-2 border border-gray-300 rounded-2xl">
-        <h2 className="text-5xl font-bold">Design Templates Pack</h2>
-        <h4 className="text-[#627382]">
-          2000+ premium templates for social media, presentations, and marketing
-          materials.
-        </h4>
-        <div className="flex items-center">
-          <h2 className="text-5xl font-bold">$49</h2>
-          <h4 className="text-xl text-[#627382]">/one-time</h4>
-        </div>
-        <h3 className="text-xl text-[#627382]">2000+ templates</h3>
-        <h3 className="text-xl text-[#627382]">Monthly updates</h3>
-        <h3 className="text-xl text-[#627382]">Commercial license</h3>
-        <button className="btn btn-primary inline-block">Primary</button>
+      <div className="grid grid-cols-3 gap-6 border border-gray-300 p-4 rounded-2xl">
+        {cardData.map((data) => {
+          return (
+            <div className="card p-8 space-y-2 border border-gray-300 rounded-2xl">
+              <h2 className="text-3xl font-bold">{data.title}</h2>
+              <h4 className="text-[#627382]">
+                {data.description}
+              </h4>
+              <div className="flex items-center">
+                <h2 className="text-5xl font-bold">{data.price}</h2>
+                <h4 className="text-xl text-[#627382]">/{data.billingCycle}</h4>
+              </div>
+              <h3 className="text-xl text-[#627382]">{data.feature1}</h3>
+              <h3 className="text-xl text-[#627382]">{data.feature2}</h3>
+              <h3 className="text-xl text-[#627382]">{data.feature3}</h3>
+              <button className="btn btn-primary inline-block">{data.buttonText}</button>
+            </div>
+          );
+        })}
       </div>
     </div>
   );
--- src/components/premiumCards/PremiumCards.jsx
+++ src/components/premiumCards/PremiumCards.jsx
@@ -2,15 +2,12 @@ import React, { use } from "react";
 import PremiumCard from "../premiumCard/PremiumCard";
 
 const PremiumCards = ({cardPromise}) => {
-  // console.log( cardPromise);
   const cardData = use(cardPromise);
 
-  console.log( cardData);
 
   return (
     <div>
-      {/* <div className="grid grid-cols-3 gap-6 border border-gray-300 p-4 rounded-2xl"></div> */}
-      
+      <PremiumCard cardData={cardData}/>
     </div>
   );
 };
